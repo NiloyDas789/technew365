8c2a21614ab536db4df3f39dc89cc541ef3aa6ba
Niloy Das <ndas4300@gmail.com>
2023-04-03 23:51:29 +0600

    Soft Delete added in course and User.

--- database/migrations/2023_04_01_055441_create_courses_table.php
+++ database/migrations/2023_04_01_055441_create_courses_table.php
@@ -19,6 +19,7 @@ return new class () extends Migration {
             $table->string('image');
             $table->boolean('status')->default(true);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
